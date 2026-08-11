8b436f72908a90b4f1ef21a42636e90e569a3818
jackgusler <guslerj1@newpaltz.edu>
2026-08-11 08:38:11 -0400

    Auto-backup: 2026-08-11 12:38:11

--- scripts/item.gd
+++ scripts/item.gd
@@ -5,9 +5,9 @@ var type: ItemType
 var tier: int = 0
 var value: float = 0.0
 
-static func create(item_type: ItemType, tier: int = 0) -> Item:
+static func create(item_type: ItemType, item_tier: int = 0) -> Item:
 	var it = Item.new()
 	it.type = item_type
-	it.tier = tier
-	it.value = item_type.tier_values[tier]
+	it.tier = item_tier
+	it.value = item_type.tier_values[item_tier]
 	return it
